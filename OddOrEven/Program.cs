fbb810f9f293148b5becba14df97d0f85bd825e2
Chr P <christian.pintson@tktk.ee>
2023-02-02 12:59:45 +0200

    first

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("enter a number!");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//k as kasutaja arv on paaris või paaritu
+
+int result = userNum % 2;
+
+Console.WriteLine(result);
+
+if (result != 0)
+
+{
+    Console.WriteLine("User number is odd");
+}
+
+else
+{
+    Console.WriteLine("User number is even");
+}
+
+
+
---
+++ Week3_IfAndSwitch/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 28;
+int b = 7;
+
+int c = b % a;
+
+Console.WriteLine(c);
+
+
